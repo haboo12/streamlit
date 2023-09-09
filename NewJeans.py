00111429976fe9ff2b29a916dcfe043457f63185
haboo12 <144503318+haboo12@users.noreply.github.com>
2023-09-09 11:46:33 +0900

    Update NewJeans.py

--- NewJeans.py
+++ NewJeans.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-image_path = '캡처.png'
+font_dirs = [os.getcwd() + '/customFonts']
+font_files = fm.findSystemFonts(fontpaths=font_dirs)
+
+for font_file in font_files:
+    fm.fontManager.addfont(font_file)
+fm._load_fontmanager(try_read_cache=False)
+
+image_path = '캡처.PNG'
 
 st.image(image_path)
 
