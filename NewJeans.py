1712eae7542c474a20df8d78b39b85cd2a431bc4
haboo12 <144503318+haboo12@users.noreply.github.com>
2023-09-09 11:48:31 +0900

    Update NewJeans.py

--- NewJeans.py
+++ NewJeans.py
@@ -1,13 +1,5 @@
 import streamlit as st
-import os
-import matplotlib.font_manager as fm
 
-font_dirs = [os.getcwd() + '/customFonts']
-font_files = fm.findSystemFonts(fontpaths=font_dirs)
-
-for font_file in font_files:
-    fm.fontManager.addfont(font_file)
-fm._load_fontmanager(try_read_cache=False)
 
 image_path = '캡처.PNG'
 
