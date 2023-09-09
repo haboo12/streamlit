b1418dc4311096e3cde2e5dd5070a7dbd76bc126
haboo12 <144503318+haboo12@users.noreply.github.com>
2023-09-09 11:52:16 +0900

    Update album.py

--- pages/album.py
+++ pages/album.py
@@ -3,15 +3,10 @@ st.title(":star: NewJeans Photo Album :star:")
 st.subheader("**뉴진스 사진첩**")
 
 st.image('Nj_GetUp_8.jpg', caption='뉴진스 단체사진')
-
 st.image('NJ_GetUp_10.jpg', caption='super shy 하니')
-
 st.image('NJ_GetUp_11.jpg', caption='super shy 민지')
-
 st.image('NJ_GetUp_12.jpg', caption='super shy 다니')
-
 st.image('NJ_GetUp_13.jpg', caption='super shy 해린')
-
 st.image('NJ_GetUp_14.jpg', caption='super shy 혜인')
 
 
