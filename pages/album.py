ec9b800c810cef142b93a7e47933038f8033ef5e
haboo12 <144503318+haboo12@users.noreply.github.com>
2023-09-09 11:54:10 +0900

    Update album.py

--- pages/album.py
+++ pages/album.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title(":star: NewJeans Photo Album :star:")
 st.subheader("**뉴진스 사진첩**")
 
-st.image('Nj_GetUp_8.jpg', caption='뉴진스 단체사진')
+st.image('NJ_GetUp_8.jpg', caption='뉴진스 단체사진')
 st.image('NJ_GetUp_10.jpg', caption='super shy 하니')
 st.image('NJ_GetUp_11.jpg', caption='super shy 민지')
 st.image('NJ_GetUp_12.jpg', caption='super shy 다니')
