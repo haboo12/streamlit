212a95bfc93edfb16fc5f03992c35d2dcdf79679
haboo12 <144503318+haboo12@users.noreply.github.com>
2023-09-09 12:18:55 +0900

    Update album.py

--- pages/album.py
+++ pages/album.py
@@ -9,6 +9,22 @@ st.image('NJ_GetUp_12.jpg', caption='super shy 다니')
 st.image('NJ_GetUp_13.jpg', caption='super shy 해린')
 st.image('NJ_GetUp_14.jpg', caption='super shy 혜인')
 
+st.image('NJ_2.jpg', caption='ETA 뉴진스')
 
+st.image('NJ_GetUp_2.jpg', caption='super shy 해린')
+st.image('NJ_GetUp_3.jpg', caption='super shy 혜인')
+st.image('NJ_GetUp_4.jpg', caption='super shy 다니')
+st.image('NJ_GetUp_5.jpg', caption='super shy 민지')
+st.image('NJ_GetUp_6.jpg', caption='super shy 하니')
 
+st.image('NJ_1.jpg', caption='뉴진스 단체사진')
 
+st.image('NJ_danni1.jpg', caption='다니엘')
+st.image('NJ_danni2.jpg', caption='다니엘')
+st.image('NJ_hanni1.jpg', caption='팜하니')
+st.image('NJ_minji1.jpg', caption='김민지')
+st.image('NJ_minji2.jpg', caption='김민지')
+st.image('NJ_harine1.jpg', caption='강해린')
+st.image('NJ_harine2.jpg', caption='강해린')
+st.image('NJ_hyein1.jpg', caption='이혜인')
+st.image('NJ_hyein2.jpg', caption='이혜인')
